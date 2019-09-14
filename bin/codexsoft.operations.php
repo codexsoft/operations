f3b4437fd6d82b62b096b27d9bca133989c079f6
Dmitriy V Kozubskiy <kozubskiyd@vezubr.ru>
2019-09-14 07:44:00 +0700

    fixed bin scripts

--- bin/codexsoft.operations.php
+++ bin/codexsoft.operations.php
@@ -1,75 +1,22 @@
 <?php
 
-use CodexSoft\Code\Context\Context;
 use CodexSoft\Code\Helpers\Cli;
 use CodexSoft\OperationsSystem\OperationsSystemSchema;
 use Symfony\Component\Console\Command\Command;
 
 require_once __DIR__.'/findautoloader.php';
-$domainConfigFile = Cli::getFirstArgumentOrDie();
-$domainSchema = OperationsSystemSchema::getFromConfigFile($domainConfigFile);
-
-//$console = ConsoleRunner::createApplication($ormSchema, $ormConfigFile, __FILE__);
-
-$console = new \Symfony\Component\Console\Application('CodexSoft Domain tools CLI');
+$operationsSystemConfigFile = Cli::getFirstArgumentOrDie();
+$operationsSystemSchema = OperationsSystemSchema::getFromConfigFile($operationsSystemConfigFile);
+$console = new \Symfony\Component\Console\Application('CodexSoft operations tools CLI');
 
 $commandList = [
 
     'add-operation' => (new \CodexSoft\OperationsSystem\Command\GenerateOperationCommand)
-        ->setDomainSchema($domainSchema),
+        ->setOperationsSystemSchema($operationsSystemSchema),
 
     'selfcheck' => (new \CodexSoft\OperationsSystem\Command\SelfCheckCommand)
-        ->setDomainSchema($domainSchema),
+        ->setOperationsSystemSchema($operationsSystemSchema),
 
-    //'repos' => new ExecuteOperationCommand(
-    //    (new \CodexSoft\DatabaseFirst\Operation\GenerateReposOperation)->setDoctrineOrmSchema($ormSchema)
-    //),
-    //
-    //'models' => new ExecuteOperationCommand(
-    //    (new \CodexSoft\DatabaseFirst\Operation\GenerateEntitiesOperation)->setDoctrineOrmSchema($ormSchema)
-    //),
-    //
-    //'add-migration' => new ExecuteOperationCommand(
-    //    (new \CodexSoft\DatabaseFirst\Operation\GenerateMigrationOperation)->setDoctrineOrmSchema($ormSchema)
-    //),
-    //
-    //'mapping' => new ExecuteShellCommand([
-    //    'php '.$cliDir.'/doctrine.orm.php '.$ormConfigFile.' orm:convert-mapping '
-    //    .Constants::CUSTOM_CODEXSOFT_BUILDER.' '
-    //    .$ormSchema->getPathToMapping().' '
-    //    .'--force --from-database --namespace='.$ormSchema->getNamespaceModels().'\\'
-    //]),
-    //
-    //'migrate' => new ExecuteShellCommand([
-    //    'php '.$cliDir.'/doctrine.migrate.php '.$ormConfigFile.' migrations:migrate',
-    //]),
-    //
-    //'check' => new ExecuteShellCommand([
-    //    'php '.$cliDir.'/doctrine.orm.php '.$ormConfigFile.' orm:validate-schema --skip-sync',
-    //]),
-    //
-    //'review' => new ExecuteShellCommand([
-    //    'php '.$cliFile.' '.$ormConfigFile.' mapping',
-    //    'php '.$cliFile.' '.$ormConfigFile.' models',
-    //    'php '.$cliFile.' '.$ormConfigFile.' repos',
-    //]),
-    //
-    //'regenerate' => new ExecuteShellCommand([
-    //    'php '.$cliFile.' '.$ormConfigFile.' db-clean',
-    //    'php '.$cliDir.'/doctrine.migrate.php '.$ormConfigFile.' migrations:migrate --no-interaction',
-    //    'php '.$cliFile.' '.$ormConfigFile.' review',
-    //    'php '.$cliFile.' '.$ormConfigFile.' check',
-    //]),
-
-    //'db-clean' => new ExecuteClosureCommand(function(Command $cmd, InputInterface $input, OutputInterface $output) use ($ormSchema) {
-    //    Database::deleteAllUserTables($ormSchema->getEntityManager()->getConnection());
-    //}),
-    //
-    //'db-truncate' => new ExecuteClosureCommand(function(Command $cmd, InputInterface $input, OutputInterface $output) use ($ormSchema) {
-    //    Database::truncateAllUserTables($ormSchema->getEntityManager()->getConnection());
-    //}),
-    //
-    //'uuid' => \App\Command\GetUuidCommand::class, // ok
 ];
 
 foreach ($commandList as $command => $commandClass) {
@@ -87,14 +34,6 @@ foreach ($commandList as $command => $commandClass) {
     };
 
 }
-
-Context::create([
-    //new class extends AbstractDomain {}
-    (new \App\TheDomain), // just to provide now() and other shourtcuts functionality... todo: remove!
-    new \CodexSoft\OperationsSystem\OperationsProcessor,
-]);
-
-\App\Shortcuts::register();
 
 /** @noinspection PhpUnhandledExceptionInspection */
 $console->run();
